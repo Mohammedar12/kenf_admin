f729d1b3c7111222aa041dfbd7c315777e5ac72c
Mohammedar12 <alikkar87@gmail.com>
2023-06-07 17:27:39 +0300

    brands

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: true,
+  defaultauth: 'fackbackend',
+  backend: 'https://kenf.sa/api',
+  imageBackend: 'https://kenf.sa',
+  // backend: 'http://localhost:3000/api',
+  firebaseConfig: {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
+  }
+};
